3e09f01919f1cfe8a7747169c5982dc48a064673
tamaraKF <tamararesendeficher@gmail.com>
2025-03-28 20:44:45 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,53 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main () {
+char estado [3]; // por exemplo: "SP", "RJ"
+int codigo_carta;
+char nome_da_cidade[50];
+int população;
+float area_km;
+float PIB;
+int  quantidades_pontos_turísticos;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+printf("código da carta: ");
+    scanf ("%d", &codigo_carta);
 
-    return 0;
+'\n'
+
+printf("digite o nome do estado (ex: SP): ");
+    scanf("%s", estado);
+
+"\n"
+getchar(); // limpa o '\n' deixando no buffer
+
+printf("nome da cidade:");
+    fgets(nome_da_cidade, 50, stdin);
+
+"\n"
+
+printf("área em km²:");
+    scanf("%f", &area_km);
+
+"\n"
+
+printf("PIB:");
+    scanf("%f", &PIB);
+
+"\n"
+
+prinf("quantidade de ponstos turisticos:");
+    scanf("%d", &quantida_pontos_turisticos);
+
+//exibindo os dados lidos
+
+printf("\n--- Dados lidos ---\n");
+printf("Codigo da carta: %d\n", codigo_carta);
+printf("Estado: %s\n", estado);
+printf("Cidade: %s", nome_da_cidade); // ja tem "\n" embutido no fgets
+printf("Area: %.2f km²\n", area_km);
+printf("PIB: %.2f\n", PIB);
+printf("Pontos turisticos: %d\n", quantidades_pontos_turísticos);
+
+return 0;
+
 }
